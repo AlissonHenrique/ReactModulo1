19e50bb03199048d83fb5327b22855bff8f5ee6b
alisson <alisson-henri@live.com>
2019-05-02 15:56:07 -0300

    Finish modulo1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-alert("hello");
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import Button from './Button';
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  handleClick = () => {
+    this.setState({ counter: +1 });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Titulo</h1>
+        <Button
+          onClick={() => {
+            alert('teste');
+          }}
+        />
+        <Button onClick={this.handleClick}> Botao</Button>
+
+        {this.state.counter}
+      </Fragment>
+    );
+  }
+}
+render(<App />, document.getElementById('app'));
